98f0a086e597541e503272c6f6dc34906c513f16
Alex Thompson <godfoder@acis.ufl.edu>
2014-10-08 15:15:11 -0400

    error handling in view.

--- app/controllers/view.js
+++ app/controllers/view.js
@@ -14,41 +14,44 @@ module.exports = function(app, config) {
             var uuid = req.params.uuid;
 
             request.get(config.search.server + config.search.index + t + "/" + uuid,function (error, response, body) {
-                // console.log(body)
                 var body = JSON.parse(body);
 
-                console.log(JSON.stringify(body,null,2));
-
-
-                var indexterms = _.cloneDeep(body._source);
-                delete indexterms["data"]
-                var rb = {
-                    "uuid": body._id,
-                    "etag": body._source.data["idigbio:etag"],
-                    "version": body._source.data["idigbio:version"],
-                    "data": body._source.data["idigbio:data"],
-                    "recordIds": body._source.data["idigbio:recordIds"],
-                    "indexTerms": indexterms,
-                    "attribution": {}
-                };
-
-                if (body._source.recordset){
-                    var rs = {
-                        "uuid": body._source.recordset
+                if (body.found) {
+                    var indexterms = _.cloneDeep(body._source);
+                    delete indexterms["data"]
+                    var rb = {
+                        "uuid": body._id,
+                        "etag": body._source.data["idigbio:etag"],
+                        "version": body._source.data["idigbio:version"],
+                        "data": body._source.data["idigbio:data"],
+                        "recordIds": body._source.data["idigbio:recordIds"],
+                        "indexTerms": indexterms,
+                        "attribution": {}
                     };
-                    if (config.recordsets[body._source.recordset]) {
-                        _.defaults(rs,config.recordsets[body._source.recordset])
-                        rb.attribution = rs;
-                        res.json(rb);
-                    } else {
-                        loadRecordsets(function(){
+
+                    if (body._source.recordset){
+                        var rs = {
+                            "uuid": body._source.recordset
+                        };
+                        if (config.recordsets[body._source.recordset]) {
                             _.defaults(rs,config.recordsets[body._source.recordset])
                             rb.attribution = rs;
                             res.json(rb);
-                        });
+                        } else {
+                            loadRecordsets(function(){
+                                _.defaults(rs,config.recordsets[body._source.recordset])
+                                rb.attribution = rs;
+                                res.json(rb);
+                            });
+                        }
+                    } else {
+                        res.json(rb);
                     }
                 } else {
-                    res.json(rb);                    
+                    res.status(404).json({
+                        "error": "NotFound",
+                        "statusCode": 404
+                    })
                 }
             })
         },        
