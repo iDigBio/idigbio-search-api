d7c65107d6866dca62bc1bd3ba74197e75b1d52b
Alex Thompson <godfoder@acis.ufl.edu>
2015-01-20 12:28:23 -0500

    count summaries.

--- app/lib/query-generators.js
+++ app/lib/query-generators.js
@@ -4,6 +4,27 @@ module.exports = function(app,config) {
 
     var queryShim = require('../lib/query-shim.js')(app,config);
 
+    function hasTerms(path, d) {
+        if(isKeyDefined(path, d)) {
+            path.forEach(function(k){
+                d = d[k];
+            });
+            return Object.keys(d).length > 0;
+        }
+        return false;
+    }
+
+    function isKeyDefined(path,wd){
+        var rv = true
+        path.forEach(function(k){
+            if (!wd[k]) {
+                rv = false;
+            }
+            wd = wd[k];
+        });
+        return rv;
+    }
+
     function media_query(rq,mq,fields,sort,limit,offset) {
         var rquery = queryShim(rq);
         var mrquery = queryShim(mq);
@@ -17,31 +38,43 @@ module.exports = function(app,config) {
             }
         };
 
-        if (mrquery["query"]["filtered"]["filter"]) {
+        if (hasTerms(["query","filtered","filter"], mrquery)) {
             query["query"]["filtered"]["filter"] = mrquery["query"]["filtered"]["filter"];
         }
 
-        if (mrquery["query"]["filtered"]["query"]) {
-            query["query"]["filtered"]["query"] = {
-                "bool": {
-                    "must": [
-                        {
-                            "has_parent" : {
-                                "parent_type" : "records",
-                                "query" : rquery["query"]
-                            }
-                        },
-                        mrquery["query"]["filtered"]["query"]
-                    ]
-                }
-            };
-        } else {
-            query["query"]["filtered"]["query"] = {
+        var recordQuery;
+
+        if (hasTerms(["query","filtered","filter"], rquery) || hasTerms(["query","filtered","query"], rquery)) {
+            recordQuery = {
                 "has_parent" : {
                     "parent_type" : "records",
                     "query" : rquery["query"]
                 }
             };
+        }
+
+        if (recordQuery) {
+            if (hasTerms(["query","filtered","query"], mrquery)) {
+                query["query"]["filtered"]["query"] = {
+                    "bool": {
+                        "must": [
+                            recordQuery,
+                            mrquery["query"]["filtered"]["query"]
+                        ]
+                    }
+                };
+            } else {
+                query["query"]["filtered"]["query"] = recordQuery;
+            }
+        } else {
+            if (hasTerms(["query","filtered","query"], mrquery)) {
+                query["query"]["filtered"]["query"] = mrquery["query"]["filtered"]["query"];
+            }
+        }
+
+
+        if (!hasTerms(["query","filtered","query"], query)) {
+            delete query["query"]["filtered"]["query"]
         }
 
         query["aggs"] = {
