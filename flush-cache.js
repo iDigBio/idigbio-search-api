d45bbe57ddc62d8b6de1bcca68d5cca98265cbc3
Alex Thompson <godfoder@acis.ufl.edu>
2015-03-26 10:40:31 -0400

    add cache flusher script.

---
+++ flush-cache.js
@@ -0,0 +1,14 @@
+var config = require("./config/config.js");
+var async = require("async");
+
+config.redis.client.keys("map_cache_*",function(err,results){
+    console.log(results);
+    async.each(results,function(key,cb){        
+        config.redis.client.del(key,function(err,results){
+            console.log(key);
+            cb()
+        })
+    },function(){
+        process.exit()
+    })
+})
