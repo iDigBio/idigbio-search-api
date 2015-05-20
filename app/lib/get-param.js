7f127ab56849e04419d7f8ab7b5e1c9661d7a568
Alex Thompson <godfoder@acis.ufl.edu>
2015-05-20 17:08:35 -0400

    support both underscored and camel case parameter naming. Fixes #7 in a backwards compatible way.

--- app/lib/get-param.js
+++ app/lib/get-param.js
@@ -3,12 +3,49 @@
 //module.exports = function(app,config) {
 module.exports = function() {
     return function(req,param,munger,def) {
-        if (req.body[param]) {
-            return munger(req.body[param]);
-        } else if (req.query[param]){
-            return munger(req.query[param]);
-        } else {
-            return def;
+        var params = [param];
+        var mangle_param;
+
+        // topFileds -> top_fields
+        if (/.*[A-Z].*/.test(param)) {
+            mangle_param = "";
+            param.split(/([A-Z])/).forEach(function(part){
+                if (/[A-Z]/.test(part)) {
+                    mangle_param += "_" + part.toLowerCase();
+                } else {
+                    mangle_param += part;
+                }
+            })
+            if (mangle_param != param) {
+                params.push(mangle_param);
+            }
         }
+
+        // top_fields -> topFields
+        if (/.*_.*/.test(param)) {
+            mangle_param = "";
+            param.split("_").forEach(function(part){
+                if (mangle_param == "") {
+                    mangle_param += part;
+                } else {
+                    mangle_param += part[0].toUpperCase() + part.substr(1);
+                }
+            });
+            if (mangle_param != param) {
+                params.push(mangle_param);
+            }
+        }
+
+        for (var i in params) {
+            var p = params[i];
+            if (req.body[p]) {
+                return munger(req.body[p]);
+            } else if (req.query[p]){
+                return munger(req.query[p]);
+            }
+        }
+
+        // return default if we haven't returned anything else
+        return def;
     };
 };
