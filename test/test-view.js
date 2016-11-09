b6f623da70214c579b211fb68c135beb400460bb
Nathan Bird <nbird@acis.ufl.edu>
2016-11-09 13:25:34 -0500

    Expand test-view
    more cases, more assertions

--- test/test-view.js
+++ test/test-view.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var _ = require('lodash');
 var should = require('chai').should();  // eslint-disable-line no-unused-vars
 var request = require('supertest'),
     app = require('../app.js');
@@ -17,6 +18,18 @@ describe('View', function() {
           done();
         });
     });
+    it('should work for recordsets', function(done) {
+      request(app.server)
+        .get("/v2/view/recordsets/6bb853ab-e8ea-43b1-bd83-47318fc4c345")
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(error, response) {
+          response.body.uuid.should.equal("6bb853ab-e8ea-43b1-bd83-47318fc4c345");
+          response.body.type.should.equal("recordsets");
+          response.body.data.collection_name.should.equal("UF Invertebrate Zoology");
+          done();
+        });
+    });
     it('should work for publishers', function(done) {
       request(app.server)
         .get("/v2/view/publishers/076c0ff6-65e9-48a5-8e4b-2447936f9a1c")
@@ -24,19 +37,24 @@ describe('View', function() {
         .expect(200)
         .end(function(error, response) {
           response.body.uuid.should.equal("076c0ff6-65e9-48a5-8e4b-2447936f9a1c");
+          response.body.type.should.equal("publishers");
           done();
         });
     });
-    it('should 404 on missing record', function(done) {
-      request(app.server)
-        .get("/v2/view/records/00000000-0000-0000-0000-000000000000")
-        .expect('Content-Type', /json/)
-        .expect(404)
-        .end(function(error, response) {
-          response.body.error.should.equal("Not Found");
-          done();
-        });
-    });
+    _.each(['publishers', 'recordsets', 'records', 'mediarecords'],
+           function(t) {
+             it('should 404 on missing ' + t, function(done) {
+               request(app.server)
+                 .get("/v2/view/" + t + "/00000000-0000-0000-0000-000000000000")
+                 .expect('Content-Type', /json/)
+                 .expect(404)
+                 .end(function(error, response) {
+                   response.body.error.should.equal("Not Found");
+                   done();
+                 });
+             });
+           }
+          );
     it('should have media aliased to mediarecords', function(done) {
       request(app.server)
         .get("/v2/view/media/00100314-3220-4107-87f3-43cfdfa0cf10")
