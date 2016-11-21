05ebf6ab6a50a90f3693c5ba3f737d6a42842377
Nathan Bird <nbird@acis.ufl.edu>
2016-11-21 16:31:57 -0500

    Respond to all errors with json

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import compress from 'koa-compress';
 import api from 'api';
 import "controllers/home";
 import "controllers/view";
+import "controllers/manage";
 // import config from './config';
 
 const compresser = compress({
@@ -19,10 +20,28 @@ const compresser = compress({
 
 const logger = morgan('combined');
 
+const jsonErrors = async function(ctx, next) {
+  try {
+    await next();
+  } catch (err) {
+    // will only respond with JSON
+    ctx.status = err.statusCode || err.status || 500;
+    ctx.body = {
+      error: err.message
+    };
+    throw err;
+  }
+  if(ctx.status === 404) {
+    ctx.body = {error: "Not Found"};
+    ctx.status = 404;
+  }
+};
+
 // TODO: Trust X-Forwarded-For proxy config
 const app = new Koa()
       .use(logger)
       .use(compresser)
+      .use(jsonErrors)
       .use(cors())
       .use(bodyParser())
       .use(api.routes())
