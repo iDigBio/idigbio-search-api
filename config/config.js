31027f620b30ed653bcf463797f28b7b987764c4
Alex Thompson <godfoder@acis.ufl.edu>
2016-12-19 10:42:50 -0500

    promote new index to production

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ var config = {
     port: 19196,
     search: {
         server: "http://c18node2-crn.acis.ufl.edu:9200",
-        index: "idigbio-2.10.3",
+        index: "idigbio-2.10.4",
         statsIndex: "stats-2.5.0",
         useEsClient: true
     },
@@ -48,7 +48,7 @@ if (process.env.NODE_ENV === "prod") {
     //    "c17node56.acis.ufl.edu:9200"
     //];
 
-    config.search.index = "idigbio-2.10.4";
+    //config.search.index = "idigbio-2.10.4";
 }
 
 config.redis.client = redis.createClient(config.redis.port,config.redis.hostname)
