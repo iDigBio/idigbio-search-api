bd4cb87d41717a78941322480f423b02286a3b5a
Alex Thompson <godfoder@acis.ufl.edu>
2016-12-19 09:15:48 -0500

    rev beta index.

--- config/config.js
+++ config/config.js
@@ -48,7 +48,7 @@ if (process.env.NODE_ENV === "prod") {
     //    "c17node56.acis.ufl.edu:9200"
     //];
 
-    //config.search.index = "idigbio-2.10.3";
+    config.search.index = "idigbio-2.10.4";
 }
 
 config.redis.client = redis.createClient(config.redis.port,config.redis.hostname)
