ed33860813be0a29d845adc23125a920f021eeb0
Nathan Bird <nbird@acis.ufl.edu>
2017-01-18 09:44:35 -0500

    Name imports more specifically
    removing the import star makes it more clear what is used.

--- src/jobs.js
+++ src/jobs.js
@@ -3,13 +3,13 @@ import _ from "lodash";
 
 import config from "config";
 
-import * as recordsets from "lib/recordsets";
-import * as indexTerms from "lib/indexTerms";
+import {loadAll as loadAllRecordsets}  from "lib/recordsets";
+import {loadIndexTerms} from "lib/indexTerms";
 import timer from "lib/timer";
 
 const jobs = [
-  {job: recordsets.loadAll, time: 1000 * 60 * 60},
-  {job: indexTerms.loadIndexTerms, time: 1000 * 60 * 60}
+  {job: loadAllRecordsets, time: 1000 * 60 * 60},
+  {job: loadIndexTerms, time: 1000 * 60 * 60}
 ];
 
 export default function startJobs() {
