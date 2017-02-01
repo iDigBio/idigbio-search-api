35e5d3470fda430201751ab642b99edb96cfe24e
Nathan Bird <nbird@acis.ufl.edu>
2017-02-01 14:34:32 -0500

    whitespace/style fixup

--- src/lib/timer.js
+++ src/lib/timer.js
@@ -3,10 +3,10 @@ import logger from "logging";
 export default function timer(...args) {
   let name = null;
   let fn = null;
-  if (args.length === 1) {
+  if(args.length === 1) {
     fn = args[0];
     name = fn.name;
-  } else if (args.length === 2) {
+  } else if(args.length === 2) {
     [name, fn] = args;
   } else {
     throw new Error("Incorrect number of arguments to timer.");
