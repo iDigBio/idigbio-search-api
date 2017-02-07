85efeaa863d15ed1fd56995d2796526b6bb06b01
Nathan Bird <nbird@acis.ufl.edu>
2017-02-07 15:23:46 -0500

    Don't patch ElasticSearch defer/promise implementation
    We try to use babel to replace the global promise as early as possible
    so if ES uses the builtin it should get bluebird anyways. Bluebird
    warns about using bluebird.defer though and that warning pops up every search.

--- src/esclient.js
+++ src/esclient.js
@@ -6,7 +6,7 @@ import config from "config";
 
 export default () => {
   const esconfig = _.cloneDeep(config.elasticsearch);
-  esconfig.defer = () => bluebird.defer();
+//  esconfig.defer = () => bluebird.defer();
   esconfig.hosts = _.shuffle(esconfig.hosts);
   return new elasticsearch.Client(esconfig);
 };
