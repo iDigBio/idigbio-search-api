efd9be1fca6e74901efcabc374b725e356f96cb5
Dan Stoner <danstoner@gmail.com>
2018-10-23 20:50:57 -0400

    add additional config info to the healthz endpoint

--- src/api.js
+++ src/api.js
@@ -9,10 +9,14 @@ const api = KoaRouter();
 api.get('/healthz', function(ctx) {
   ctx.cacheControl(0);
   ctx.body = {
-    ENV: config.ENV,
-    path: ctx.originalUrl,
-    version: process.env.npm_package_version,
-    index: config.search.index
+      ENV: config.ENV,
+      path: ctx.originalUrl,
+      version: process.env.npm_package_version,
+      index: config.search.index,
+      maxRecordsets: config.maxRecordsets,
+      defaultLimit: config.defaultLimit,
+      maxLimit: config.maxLimit,
+      maxTileObjects: config.maxTileObjects
   };
 });
 
