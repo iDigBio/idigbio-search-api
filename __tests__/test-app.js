1c1848cc1042a8deeaf13142f8e6d7f5d63b1036
Ubuntu <ubuntu@idb-api22-prod.acis.ufl.edu>
2023-07-10 18:05:00 +0000

    put module test back in place

--- __tests__/test-app.js
+++ __tests__/test-app.js
@@ -3,17 +3,9 @@ jest.mock('esclient');
 
 describe('app', function() {
   it("should be importable", async function() {
-    // Mock the 'app' module
-    const app = {};
-
-    // Mock the 'ready' property as a resolved Promise
-    app.ready = Promise.resolve();
-
-    // Assert that the 'app' module is importable
-    expect(app).toBeDefined();
-
-    // Await the 'ready' promise to ensure the module becomes ready
+    // All the other tests import it outside of a test. In that case if
+    // there is an error then jest doesn't catch that well.
+    const app = require('app');
     await app.ready;
-
   });
 });
